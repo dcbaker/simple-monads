196ba9989fcc9f23a7c11e8bfd75ef3f5ed7d064
Dylan Baker <dylan@pnwbakers.com>
2023-12-14 13:55:54 -0800

    result: add unwrap method

--- pyadt/result.py
+++ pyadt/result.py
@@ -13,13 +13,27 @@ E = TypeVar('E', bound=Exception)
 __all__ = [
     'Result',
     'Error',
-    'Nothing',
+    'Success',
+    'UnwrapError',
 ]
+
+
+class UnwrapError(Exception):
+    pass
 
 
 class Result(Generic[T, E]):
 
     """Base Class for Option, do not directly instantiate"""
+
+    def unwrap(self, msg: str | None = None) -> T:
+        """Get the held value or throw an Exception.
+
+        :param msg: The error message, otherwise a default is used
+        :raises UnwrapError: If this is an Err
+        :return: The held value
+        """
+        raise NotImplementedError()
 
 
 @dataclass(slots=True, frozen=True)
@@ -27,8 +41,14 @@ class Error(Result[T, E]):
 
     _held: E
 
+    def unwrap(self, msg: str | None = None) -> T:
+        raise UnwrapError(msg or 'Attempted to unwrap an Error') from self._held
+
 
 @dataclass(slots=True, frozen=True)
 class Success(Result[T, E]):
 
     _held: T
+
+    def unwrap(self, msg: str | None = None) -> T:
+        return self._held
--- tests/test_result.py
+++ tests/test_result.py
@@ -1,2 +1,26 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2023 Dylan Baker
+
+
+from __future__ import annotations
+
+import pytest
+
+from pyadt.result import *
+
+
+class TestResult:
+
+    class TestUnwrap:
+
+        def test_error(self) -> None:
+            with pytest.raises(UnwrapError, match='Attempted to unwrap an Error'):
+                Error(Exception('foo')).unwrap()
+
+        def test_error_msg(self) -> None:
+            msg = 'test message'
+            with pytest.raises(UnwrapError, match=msg):
+                Error(Exception('foo')).unwrap(msg)
+
+        def test_success(self) -> None:
+            assert Success('foo').unwrap() == 'foo'

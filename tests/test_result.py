511048fd0a91a96be36d3fef17d154bb6f0e5869
Dylan Baker <dylan@pnwbakers.com>
2024-05-30 15:54:55 -0700

    result: add asynchronous methods

--- tests/test_result.py
+++ tests/test_result.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright © 2023 Dylan Baker
+# Copyright © 2023-2024 Dylan Baker
 
 from __future__ import annotations
 from typing import cast
@@ -41,6 +41,22 @@ class TestResult:
 
         def test_success(self) -> None:
             assert Success('foo').unwrap_or_else(lambda: 'bar') == 'foo'
+
+    class TestUnwrapOrElseAsync:
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            async def fb() -> str:
+                return 'bar'
+
+            e: Result[str, Exception] = Error(Exception('foo'))
+            assert await e.unwrap_or_else_async(fb) == 'bar'
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            async def fb() -> str:
+                return 'bar'
+            assert await Success('foo').unwrap_or_else_async(fb) == 'foo'
 
     class TestBool:
 
@@ -94,6 +110,23 @@ class TestResult:
         def test_success(self) -> None:
             assert Success('1').map(int) == Success(1)
 
+    class TestMapAsync:
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            async def cb(v: str) -> int:
+                pytest.fail()
+
+            e: Result[str, Exception] = Error(Exception('foo'))
+            assert await e.map_async(cb) == cast(Result[int, Exception], e)
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            async def cb(v: str) -> int:
+                return int(v)
+
+            assert await Success('1').map_async(cb) == Success(1)
+
     class TestMapErr:
 
         def test_error(self) -> None:
@@ -103,6 +136,22 @@ class TestResult:
         def test_success(self) -> None:
             s: Success[str, str] = Success('1')
             assert s.map_err(int) == cast(Success[str, int], s)
+
+    class TestMapErrAsync:
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            async def cb(v: str) -> int:
+                return int(v)
+            e: Result[str, str] = Error('1')
+            assert await e.map_err_async(cb) == Error(1)
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            async def cb(v: str) -> int:
+                pytest.fail()
+            s: Success[str, str] = Success('1')
+            assert await s.map_err_async(cb) == cast(Success[str, int], s)
 
     class TestMapOr:
 
@@ -114,6 +163,23 @@ class TestResult:
             s: Success[str, str] = Success('1')
             assert s.map_or('foo', lambda _: 'bar') == 'bar'
 
+    class TestMapOrAsync:
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            async def cb(v: str) -> str:
+                pytest.fail()
+
+            e: Result[str, str] = Error('1')
+            assert await e.map_or_async('foo', cb) == 'foo'
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            async def cb(v: str) -> str:
+                return 'bar'
+            s: Success[str, str] = Success('1')
+            assert await s.map_or_async('foo', cb) == 'bar'
+
     class TestMapOrElse:
 
         def test_error(self) -> None:
@@ -123,6 +189,26 @@ class TestResult:
         def test_success(self) -> None:
             s: Success[str, str] = Success('1')
             assert s.map_or_else(lambda: 'foo', lambda _: 'bar') == 'bar'
+
+    class TestMapOrElseASync:
+
+        @staticmethod
+        async def _cb(v: int) -> int:
+            return v + 5
+
+        @staticmethod
+        async def _fb() -> int:
+            return 3
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            e: Result[int, int] = Error(1)
+            assert await e.map_or_else_async(self._fb, self._cb) == 3
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            s: Success[int, str] = Success(1)
+            assert await s.map_or_else_async(self._fb, self._cb) == 6
 
     class TestAndThen:
 
@@ -138,6 +224,22 @@ class TestResult:
             s: Success[str, str] = Success('1')
             assert s.and_then(self._cb) == Success(1)
 
+    class TestAndThenAsync:
+
+        @staticmethod
+        async def _cb(res: str) -> Result[int, str]:
+            return Success(int(res))
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            e: Result[str, str] = Error('1')
+            assert await e.and_then_async(self._cb) == cast(Result[int, str], e)
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            s: Success[str, str] = Success('1')
+            assert await s.and_then_async(self._cb) == Success(1)
+
     class TestOrElse:
 
         @staticmethod
@@ -151,6 +253,22 @@ class TestResult:
         def test_success(self) -> None:
             s: Success[str, str] = Success('1')
             assert s.or_else(self._cb) == cast(Result[str, int], s)
+
+    class TestOrElseAsync:
+
+        @staticmethod
+        async def _cb(res: str) -> Result[str, int]:
+            return Error(int(res))
+
+        @pytest.mark.asyncio
+        async def test_error(self) -> None:
+            e: Result[str, str] = Error('1')
+            assert await e.or_else_async(self._cb) == Error(1)
+
+        @pytest.mark.asyncio
+        async def test_success(self) -> None:
+            s: Success[str, str] = Success('1')
+            assert await s.or_else_async(self._cb) == cast(Result[str, int], s)
 
     class TestErr:
 
@@ -227,6 +345,44 @@ class TestWrapResult:
             foo()
 
 
+class TestWrapResultASync:
+
+    @pytest.mark.asyncio
+    async def test_success(self) -> None:
+        @wrap_result_async()
+        async def foo() -> str:
+            return ''
+
+        assert await foo() == Success('')
+
+    @pytest.mark.asyncio
+    async def test_error(self) -> None:
+        @wrap_result_async(Exception)
+        async def foo() -> str:
+            raise Exception('foo')
+
+        err = await foo()
+        assert isinstance(err.unwrap_err(), Exception)
+
+    @pytest.mark.asyncio
+    async def test_error_multi(self) -> None:
+        @wrap_result_async((ValueError, RecursionError))
+        async def foo() -> str:
+            raise ValueError('foo')
+
+        err = await foo()
+        assert isinstance(err.unwrap_err(), ValueError)
+
+    @pytest.mark.asyncio
+    async def test_error_uncaught(self) -> None:
+        @wrap_result_async(ArithmeticError)
+        async def foo() -> str:
+            raise ValueError('foo')
+
+        with pytest.raises(ValueError, match='foo'):
+            await foo()
+
+
 class TestUnwrapResult:
 
     def test_success(self) -> None:
@@ -243,6 +399,26 @@ class TestUnwrapResult:
 
         with pytest.raises(ValueError, match='foo'):
             foo()
+
+
+class TestUnwrapResultAsync:
+
+    @pytest.mark.asyncio
+    async def test_success(self) -> None:
+        @unwrap_result_async
+        async def foo() -> Result[str, ValueError]:
+            return Success('foo')
+
+        assert await foo() == 'foo'
+
+    @pytest.mark.asyncio
+    async def test_error(self) -> None:
+        @unwrap_result_async
+        async def foo() -> Result[str, ValueError]:
+            return Error(ValueError('foo'))
+
+        with pytest.raises(ValueError, match='foo'):
+            await foo()
 
 
 class TestPpropgate:
@@ -266,3 +442,26 @@ class TestPpropgate:
             return x + 'bar'
 
         assert inner() == Success('foobar')
+
+
+class TestPropogateAsync:
+
+    @pytest.mark.asyncio
+    async def test_prop(self) -> None:
+        @stop_async
+        async def inner() -> str:
+            r: Result[str, int] = Error(4)
+            x = r.propagate()
+            return x + 'bar'
+
+        assert await inner() == Error(4)
+
+    @pytest.mark.asyncio
+    async def test_no_prop(self) -> None:
+        @stop_async
+        async def inner() -> str:
+            r: Result[str, int] = Success('foo')
+            x = r.propagate()
+            return x + 'bar'
+
+        assert await inner() == Success('foobar')
